494e7a8df153858ccf58eb104e4e489e8b94083e
hikari0102 <104632988+hikari0102@users.noreply.github.com>
2024-11-06 15:48:09 +0900

    Update 12731.cpp

--- 2024-10/12731.cpp
+++ 2024-10/12731.cpp
@@ -18,41 +18,41 @@ int main() {
         int na, nb, t;
         scanf("%d", &t);
         scanf("%d %d", &na, &nb);
-        vector<pair<pair<int, int>, int>> times;
-        priority_queue<int> apq;
-        priority_queue<int> bpq;
+        vector<pair<pair<int, int>, int>> times; // 시간들
+        priority_queue<int> apq; // A역 큐
+        priority_queue<int> bpq; // B역 큐
         for(int i = 0; i < na + nb; i++) {
             int a, b, c, d;
             scanf("%2d:%2d %2d:%2d", &a, &b, &c, &d);
             if(i < na)
                 times.push_back(make_pair(make_pair(a * 60 + b, c * 60 + d), 1));
             else
-                times.push_back(make_pair(make_pair(a * 60 + b, c * 60 + d), 2));
+                times.push_back(make_pair(make_pair(a * 60 + b, c * 60 + d), 2)); // 출발 시간, 도착 시간, 역 정보
         }
-        sort(times.begin(), times.end());
+        sort(times.begin(), times.end()); // 정렬
         int aans = 0, bans = 0;
         for(int i = 0; i < na + nb; i++) {
-            if(times[i].second == 1) {
+            if(times[i].second == 1) { // A->B면
                 if(!apq.empty()) {
-                    if(-(apq.top()) <= times[i].first.first)
-                        apq.pop();
+                    if(-(apq.top()) <= times[i].first.first) // A역의 큐에 출발시간보다 빨리 도착한 것이 있는가?
+                        apq.pop(); //있다면 큐에서 빼기
                     else 
-                        aans++;
+                        aans++; // 없다면 A역에서 차량 1대 출발
                 }
                 else
-                    aans++;
-                bpq.push(-(times[i].first.second + t));
+                    aans++; // 없다면 A역에서 차량 1대 출발
+                bpq.push(-(times[i].first.second + t)); // B역의 큐에 도착시간 + 회차 시간에 출발하는 열차에 push
             }
             else {
-                if(!bpq.empty()) {
-                    if(-(bpq.top()) <= times[i].first.first)
-                        bpq.pop();
+                if(!bpq.empty()) { 
+                    if(-(bpq.top()) <= times[i].first.first) //B역의 큐에 출발시간보다 빨리 도착한 것이 있는가?
+                        bpq.pop(); //있다면 큐에서 빼기
                     else 
-                        bans++;
+                        bans++;  // 없다면 B역에서 차량 1대 출발
                 }
                 else
                     bans++;
-                apq.push(-(times[i].first.second + t));
+                apq.push(-(times[i].first.second + t)); // A역의 큐에 도착시간 + 회차 시간에 출발하는 열차에 push
             }
         }
         printf("Case #%d: %d %d\n", T + 1, aans, bans);
